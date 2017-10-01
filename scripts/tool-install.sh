881ba80562c1549bfc018ce0494024a7c396d7f2
Aditya Ketkar <ketkaraditya615@gmail.com>
2017-10-01 16:13:45 +0530

    seperate dependencies from developer dependencies

--- scripts/tool-install.sh
+++ scripts/tool-install.sh
@@ -11,25 +11,3 @@ sudo apt-get install pfring nprobe ntopng ntopng-data n2disk cento nbox
 #install wireshark and tshark
 sudo apt-get install wireshark tshark
 sudo apt-get update
-
-#install bro nad tomcat7
-sudo apt-get install bro
-sudo apt-get install tomcat7
-
-#Tomcat classpath error fix
-sudo wget https://archive.apache.org/dist/logging/log4j/2.8.2/apache-log4j-2.8.2-bin.tar.gz
-sudo tar -xvzf apache-log4j-2.8.2-bin.tar.gz -C /usr/share/tomcat7/lib/
-rm apache-log4j-2.8.2-bin.tar.gz
-echo "CLASSPATH=/usr/share/tomcat7/lib/apache-log4j-2.8.2-bin/">>/usr/share/tomcat7/bin/setenv.sh
-
-#install nodejs
-curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt-get install nodejs
-
-#install maven
-sudo apt-get install maven
-
-#npm packages for front end
-cd ../src/main/webapp/WEB-INF/
-sudo npm install
