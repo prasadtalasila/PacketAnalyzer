086c2c96d412140141daaa423ea96568e872e715
Aditya Ketkar <ketkaraditya615@gmail.com>
2017-10-01 21:46:03 +0530

    Bro installation updated

--- scripts/tool-install.sh
+++ scripts/tool-install.sh
@@ -13,4 +13,13 @@ sudo apt-get install wireshark tshark
 sudo apt-get update
 
 #install bro
+sudo apt-get install cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev
 sudo apt-get install bro
+sudo wget https://www.bro.org/downloads/bro-2.5.1.tar.gz
+mkdir /opt/bro
+sudo tar -xvzf bro-2.5.1.tar.gz -C /opt/bro
+sudo apt-get clean all
+sudo apt-get update
+rm bro-2.5.1.tar.gz
+export PATH=/usr/local/bro/bin:$PATH
+
