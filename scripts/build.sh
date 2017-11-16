efbbcaf78a7cb0d199c2afbdf314b402cb772aba
Raghav Agrawal <raghav.agrawal.raghav@gmail.com>
2017-11-16 23:05:31 +0530

    Added support for Tomcat 8.5

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,10 @@ echo "] Packaging the app ..."
 mvn package
 echo "] Packaging complete."
 echo ""
-echo "] Restarting tomcat7 ..."
-sudo service tomcat7 restart
+echo "] Restarting tomcat ..."
+sudo service tomcat restart
 echo "] Restart complete."
 echo ""
 echo "] Deploying Application ..."
-mvn tomcat7:deploy
+mvn tomcat:deploy
 echo "] Deploy complete"
