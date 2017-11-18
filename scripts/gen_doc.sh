22814d8edd47c6bc4788dabd20278485f171ed48
shilparaaju <shilpa.raaju@gmail.com>
2017-11-18 13:13:58 +0530

    javadoc util edit 3

--- scripts/gen_doc.sh
+++ scripts/gen_doc.sh
@@ -1,2 +1,2 @@
-javadoc -version -author -private -d docs -classpath /home/ubuntu/darshini/src/main/java/in/ac/bits/protocolanalyzer/utils/* -sourcepath . -subpackages .
+javadoc -version -author -private -d ../docs -classpath /home/ubuntu/darshini/src/main/java/in/ac/bits/protocolanalyzer/utils/* -sourcepath . -subpackages .
 
