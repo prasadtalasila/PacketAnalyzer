ef23246be6965c5bcf3c6c77354968cd132e06f1
prasadtalasila <tsrkp@goa.bits-pilani.ac.in>
2017-11-23 05:29:42 +0530

    changes bash scripts to work with tomcat v8.5 The deployment has been changed from maven tomcat plugin to curl based deployment.

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -3,13 +3,18 @@
 echo ">> REDEPLOYING APPLICATION"
 echo ""
 echo "] Removing the war file ..."
-sudo rm -rf /var/lib/tomcat7/webapps/protocolanalyzer.war /var/lib/tomcat7/webapps/protocolanalyzer
+sudo rm -rf /opt/tomcat/webapps/protocolanalyzer.war /opt/tomcat/webapps/protocolanalyzer
 echo "] Remove complete."
 echo ""
 echo "] Restarting tomcat ..."
 sudo service tomcat restart
+sleep 5
 echo "] Restart complete."
 echo ""
+echo "] Packaging the app ..."
+mvn package
+echo "] Packaging complete."
 echo "] Deploying Application ..."
-mvn tomcat:deploy
+# mvn tomcat:deploy                     #mvn tomcat plug in does not work for tomcat v8.5
+curl --upload-file target/protocolanalyzer-1.0-SNAPSHOT.war "http://adminscript:passwordscript@localhost:8080/manager/text/deploy?path=/protocolanalyzer&update=true"
 echo "] Deploy complete"
