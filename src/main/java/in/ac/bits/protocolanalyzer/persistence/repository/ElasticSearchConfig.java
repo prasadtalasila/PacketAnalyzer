b382dbe8f50012c8ee1925949a0c5c1c50cfd753
Aditya <ketkaraditya615@gmail.com>
2018-02-03 16:30:14 +0530

    removes unused imports

--- src/main/java/in/ac/bits/protocolanalyzer/persistence/repository/ElasticSearchConfig.java
+++ src/main/java/in/ac/bits/protocolanalyzer/persistence/repository/ElasticSearchConfig.java
@@ -1,9 +1,7 @@
 package in.ac.bits.protocolanalyzer.persistence.repository;
 
 
-import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.ImmutableSettings;
-import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
